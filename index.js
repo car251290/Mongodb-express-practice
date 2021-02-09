9eb04c305d412da3b99d83b2d66ef9ea58ea1703
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-08 19:29:04 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import routes from './src/routers/crmRouter';
 import mongoose from 'mongoose';
 
 import bodyParse from 'body-parse';
-
+// this is the entry point of the server and will be express
 const app = express();
 
 const PORT = 4000;
-//mongoose connection 
-
+//mongoose connection to the API
 mongoose.Promise = global.Promise;
+//this is the connection
 mongoose.connect('mongodb://localhost/CRMdb', {
+    //this is the object on the database
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
@@ -21,11 +22,11 @@ app.use(bodyParser.uslencoded({ extended: true }));
 app.use(bodyParse.json());
 
 routes(app);
-
+// this is method get and has the variables req and res
 app.get('/', (req, res) =>
     res.send(`node and express server running on port ${PORT}`)
 );
-
+// this will past the PORT to send the messages and print the server where is running
 app.listen(PORT, () =>
     console.log(`your server is running on port ${PORT}`)
 );
