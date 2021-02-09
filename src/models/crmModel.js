b94fd6728aed4e75a6580ca7a332fd10feeaa542
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-08 19:27:47 -0800

    Add files via upload

--- src/models/crmModel.js
+++ src/models/crmModel.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-
+//create the variable for mongoose
 const Schema = mongoose.Schema;
 // the model of my database how it look
 export const ContactSchema = new Schema({
-    //list of the objects 
+    //list of the iteam for schema. 
     firstName: {
         type: String,
         required: 'Enter a frist name'
@@ -19,8 +19,11 @@ export const ContactSchema = new Schema({
         type: String
     },
     phone: {
-        type: String
+        type: Number
+    },
+    created_data: {
+        type: Date,
+        default: Date.now
     }
 
-
-})
+});
