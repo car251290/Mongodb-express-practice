74ea21a544eb08035b34183ec4593e000514f7f9
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-08 19:27:13 -0800

    Add files via upload

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongose';
 import { ContactSchema } from '../models/crmModel';
 
+// constractor and pas the shema and contacts for have new contacts
 const Contact = mongoose.model('Contact', ContactSchema);
-
+// to past function and request and res
 export const addNewContact = (req, res) => {
-    let newContact = new Contact(req.body);
-
+    const newContact = new Contact(req.body);
+    // save the contact on the database 
     newContact.save((err, contact) => {
         if (err) {
             res.send(err)
@@ -14,7 +15,7 @@ export const addNewContact = (req, res) => {
     })
 }
 
-//get contacts 
+//get contacts  on the dabata 
 export const getContact = (req, res) => {
     Contact.find({}, (err, contact) => {
         if (err) {
@@ -24,7 +25,7 @@ export const getContact = (req, res) => {
     });
 }
 
-
+// get the contact of the ID
 export const getContactwithID = (req, res) => {
     Contact.findById(req.params.contactID, (err, contact) => {
         if (err) {
@@ -34,9 +35,9 @@ export const getContactwithID = (req, res) => {
     });
 }
 
-//To make updates
+//To make updates of the contacts 
 export const updateContact = (req, res) => {
-
+    // the method to use for past the ID (key) and the request
     Contact.findOneAndUpdate({ _id: req.params.contactID }, req.body, { new: true, useFindAndModify: false }, (err, contact) => {
         if (err) {
             res.send(err)
@@ -45,12 +46,14 @@ export const updateContact = (req, res) => {
     });
 }
 
+//delete function of the contacts
 export const deleteContact = (req, res) => {
-
+    //to remove the contact we need the ID key and get the request of the params on the contactID
     Contact.remove({ _id: req.params.contactID }, (err, contact) => {
         if (err) {
             res.send(err)
         }
+        // the messaes object that the contact is delate
         res.json({ message: 'successfull delete contact' })
     });
 }
